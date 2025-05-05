2f6e2f5d18884c70421ba084c923aa6fe6c4e23b
nabila.kh <nabila.kh@nusantara.technology>
2025-05-05 14:48:25 +0700

    setup deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Header from "./components/Header";
 
 function App() {
   const [inputValue, setInputValue] = useState({
-    initial: 0,
-    annual: 0,
-    expected: 0,
-    duration: 0,
+    initial: "",
+    annual: "",
+    expected: "",
+    duration: "",
   });
 
   function handleInput(name, value) {
--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -8,30 +8,30 @@ export default function ResultList({ inputValue }) {
     duration: inputValue.duration,
   });
 
-  console.log(list);
-
   return (
-    <table id="result">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Investment Value</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map((itm) => (
+    list.length > 0 && (
+      <table id="result">
+        <thead>
           <tr>
-            <td>{itm.year}</td>
-            <td>{formatter.format(itm.valueEndOfYear)}</td>
-            <td>{formatter.format(itm.interest)}</td>
-            <td>{itm.year}</td>
-            <td>{formatter.format(itm.valueEndOfYear - itm.interest)}</td>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {list.map((itm, idx) => (
+            <tr key={idx}>
+              <td>{itm.year}</td>
+              <td>{formatter.format(itm.valueEndOfYear)}</td>
+              <td>{formatter.format(itm.interest)}</td>
+              <td>{itm.year}</td>
+              <td>{formatter.format(itm.valueEndOfYear - itm.interest)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
   );
 }
